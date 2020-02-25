9109a0dd88229dd5d8b248f2e682e019edff866e
Murilo Araujo <muernandes@icloud.com>
2020-02-25 00:40:47 -0300

    update menu icons

--- CLImageEditor/ImageTools/ToolSettings/CLToolbarMenuItem.m
+++ CLImageEditor/ImageTools/ToolSettings/CLToolbarMenuItem.m
@@ -22,13 +22,13 @@
         self.iconImageContentMode = UIViewContentModeScaleAspectFill;
         CGFloat W = frame.size.width;
         
-        _iconView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 5, W-20, W-20)];
+        _iconView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, W-40, W-40)];
         _iconView.clipsToBounds = YES;
         _iconView.layer.cornerRadius = 5;
-        _iconView.contentMode = self.iconImageContentMode;
+        _iconView.contentMode = UIViewContentModeScaleAspectFit;
         [self addSubview:_iconView];
         
-        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, _iconView.bottom + 5, W, 15)];
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, _iconView.bottom + 10, W, 15)];
         _titleLabel.backgroundColor = [UIColor clearColor];
         _titleLabel.textColor = [CLImageEditorTheme toolbarTextColor];
         _titleLabel.font = [CLImageEditorTheme toolbarTextFont];
